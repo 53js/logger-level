ecac1da23e14f6e02a74443acf2cc15683290894
Hugo Mallet <hugo.mallet@53js.fr>
2016-08-04 09:53:14 +0200

    fix line number

--- index.js
+++ index.js
@@ -1,96 +1,43 @@
 'use strict';
 
-const logger = {
-	set level(lvl) {
+const LEVEL = [
+	['ERROR', 0, console.error, 'error'],
+	['WARN', 1, console.warn, 'warn'],
+	['INFO', 2, console.info, 'info'],
+	['VERBOSE', 3, console.log, 'verbose'],
+	['DEBUG', 4, console.log, 'debug'], // Error stacktrace displayed at this level
+	['SILLY', 5, console.log, 'silly']
+];
+
+const config = {};
+
+config.log_level = LEVEL[0];
+config.show_date = true;
+config.show_time = true;
+config.show_type = true;
+
+Object.defineProperty(config, 'level', {
+	get: function level() {
+		return this.log_level;
+	},
+	set: function level(lvl) {
 		if (typeof lvl === 'string') {
 			lvl = lvl.toLowerCase();
-			switch (lvl) {
-			case 'error': lvl = logger.ERROR; break;
-			case 'warn': lvl = logger.WARN; break;
-			case 'info': lvl = logger.INFO; break;
-			case 'verbose': lvl = logger.VERBOSE; break;
-			case 'debug': lvl = logger.DEBUG; break;
-			case 'silly': lvl = logger.SILLY; break;
-			default: lvl = logger.ERROR;
-			}
+			lvl = LEVEL.find(function each(l) {
+				return l[3] === lvl;
+			});
 		}
-		logger.log_level = lvl;
-	},
-
-	get level() {
-		switch (logger.log_level) {
-		case logger.ERROR: return 'error';
-		case logger.WARN: return 'warn';
-		case logger.info: return 'info';
-		case logger.VERBOSE: return 'verbose';
-		case logger.DEBUG: return 'debug';
-		case logger.SILLY: return 'silly';
-		default: return undefined;
-		}
-	},
-
-	set browser(bool) {
-		logger.show_date =
-			logger.show_time =
-				logger.show_type = !bool;
-	},
-
-	/* eslint-disable no-use-before-define */
-	get error() {
-		if (logger.log_level >= logger.ERROR) {
-			return log.bind(this, console.error.bind(console), logger.ERROR);
-		}
-		return reject;
-	},
-
-	get warn() {
-		if (logger.log_level >= logger.WARN) {
-			return log.bind(this, console.warn.bind(console), logger.WARN);
-		}
-		return reject;
-	},
-
-	get info() {
-		if (logger.log_level >= logger.INFO) {
-			return log.bind(this, console.info.bind(console), logger.INFO);
-		}
-		return reject;
-	},
-
-	get verbose() {
-		if (logger.log_level >= logger.VERBOSE) {
-			return log.bind(this, console.log.bind(console), logger.VERBOSE);
-		}
-		return reject;
-	},
-
-	get debug() {
-		if (logger.log_level >= logger.DEBUG) {
-			return log.bind(this, console.log.bind(console), logger.DEBUG);
-		}
-		return reject;
-	},
-
-	get silly() {
-		if (logger.log_level >= logger.SILLY) {
-			return log.bind(this, console.log.bind(console), logger.SILLY);
-		}
-		return reject;
+		this.log_level = lvl;
 	}
-	/* eslint-enable no-use-before-define */
-};
+});
 
-logger.ERROR = 0;
-logger.WARN = 1;
-logger.INFO = 2;
-logger.VERBOSE = 3;
-logger.DEBUG = 4; // Error stacktrace displayed at this level
-logger.SILLY = 5;
-
-logger.log_level = logger.ERROR;
-logger.show_date = true;
-logger.show_time = true;
-logger.show_type = true;
+Object.defineProperty(config, 'browser', {
+	set: function browser(bool) {
+		this.show_date =
+			this.show_time =
+				this.show_type = !bool;
+	}
+});
 
 function getDateString() {
 	var date = new Date();
@@ -106,25 +53,25 @@ function getTimeString() {
 		('0' + date.getSeconds()).slice(-2);
 }
 
-function getTypeString(type) {
+function getTypeString(level) {
 	let result = '';
-	switch (type) {
-	case logger.ERROR:
+	switch (level[0]) {
+	case 'ERROR':
 		result = '\u001b[31;1m[error]\u001b[39;0m';
 		break;
-	case logger.WARN:
+	case 'WARN':
 		result = '\u001b[33;1m[warn]\u001b[39;0m ';
 		break;
-	case logger.INFO:
+	case 'INFO':
 		result = '\u001b[34;1m[info]\u001b[39;0m ';
 		break;
-	case logger.VERBOSE:
+	case 'VERBOSE':
 		result = '[verb] ';
 		break;
-	case logger.DEBUG:
+	case 'DEBUG':
 		result = '[debug]';
 		break;
-	case logger.SILLY:
+	case 'SILLY':
 		result = '[silly]';
 		break;
 	default:
@@ -133,25 +80,25 @@ function getTypeString(type) {
 	return result;
 }
 
-function getPrefixString(type) {
+function getPrefixString(level) {
 	let result = '';
-	if (logger.show_date) {
+	if (config.show_date) {
 		result += getDateString() + ' ';
 	}
-	if (logger.show_time) {
+	if (config.show_time) {
 		result += getTimeString() + ' ';
 	}
-	if (logger.show_type) {
-		result += getTypeString(type) + ' ';
+	if (config.show_type) {
+		result += getTypeString(level) + ' ';
 	}
 	return result;
 }
 
-function getFullLog(type, args) {
+function getFullLog(level, args) {
 	let str = args[0];
 	args = args.slice(1);
-	let prefixString = getPrefixString(type);
-	str = (type === logger.ERROR && str && str.stack) ?
+	let prefixString = getPrefixString(level);
+	str = (level[0] === LEVEL[0][0] && str && str.stack) ?
 			str.stack :
 			str;
 	let result = [];
@@ -167,11 +114,30 @@ function getFullLog(type, args) {
 	return result;
 }
 
-function reject() {}
-
-function log(consoleFunc, type) {
-	return consoleFunc.apply(console,
-		getFullLog(type, Array.prototype.slice.call(arguments, 2)));
+function reject() {
+	return function cancel() {};
 }
 
-module.exports = exports = logger;
+function getLoggers(args) {
+	var logger = {};
+	LEVEL.forEach(function each(level) {
+		Object.defineProperty(logger, level[3], {
+			get: function get() {
+				if (config.level[1] >= level[1]) {
+					return Function.prototype.bind.apply(level[2],
+						[console].concat(getFullLog(level, args)));
+				}
+				return reject;
+			}
+		});
+	});
+	return logger;
+}
+
+function log() {
+	var args = Array.prototype.slice.call(arguments);
+	return getLoggers(args);
+}
+
+module.exports = exports = log;
+exports.config = config;
--- sample.js
+++ sample.js
@@ -1,57 +1,57 @@
-var logger = require('./index');
+var log = require('./index');
 
-function log() {
+function doLog() {
 	var error = new TypeError('a type error');
-	logger.error(error);
-	logger.error('this is an error');
-	logger.warn('this is a warn');
-	logger.info('this is an info');
-	logger.verbose('this is a verbose log');
-	logger.debug('this is a debug log');
-	logger.silly('this is a silly log');
+	log(error).error();
+	log('this is an error').error();
+	log('this is a warn').warn();
+	log('this is an info').info();
+	log('this is a verbose log').verbose();
+	log('this is a debug log').debug();
+	log('this is a silly log').silly();
 }
 
-console.info('----logger.level = logger.ERROR');
-logger.level = logger.ERROR;
-log();
+console.info('---- log.config.level = ERROR (default)');
+// log.config.level = 'error';
+doLog();
 
-console.info('----logger.level = logger.WARN');
-logger.level = logger.WARN;
-log();
+console.info('---- log.config.level = WARN');
+log.config.level = 'warn';
+doLog();
 
-console.info('----logger.level = logger.INFO');
-logger.level = logger.INFO;
-log();
+console.info('---- log.config.level = INFO');
+log.config.level = 'info';
+doLog();
 
-console.info('----logger.level = logger.VERBOSE');
-logger.level = logger.VERBOSE;
-log();
+console.info('---- log.config.level = VERBOSE');
+log.config.level = 'verbose';
+doLog();
 
-console.info('----logger.level = logger.DEBUG');
-logger.level = logger.DEBUG;
-log();
+console.info('---- log.config.level = DEBUG');
+log.config.level = 'debug';
+doLog();
 
-console.info('----logger.level = logger.SILLY');
-logger.level = logger.SILLY;
-log();
+console.info('---- log.config.level = SILLY');
+log.config.level = 'silly';
+doLog();
 
-console.info('----show_date = false');
-logger.show_date = false;
-log();
+console.info('---- log.config.show_date = false');
+log.config.show_date = false;
+doLog();
 
-console.info('----show_time = false');
-logger.show_time = false;
-log();
+console.info('---- log.config.show_time = false');
+log.config.show_time = false;
+doLog();
 
-console.info('----show_type = false');
-logger.show_type = false;
-log();
+console.info('---- log.config.show_type = false');
+log.config.show_type = false;
+doLog();
 
 console.info('---- browser = false');
-logger.browser = false;
-log();
+log.config.browser = false;
+doLog();
 
 console.info('---- browser = true');
-logger.browser = true;
-log();
+log.config.browser = true;
+doLog();
 
